053e8af3d8af6ed29ef29f2c908b6207e3fb9c75
Akshatchourey <achourey09@gmail.com>
2023-10-06 22:59:23 +0530

    Most efficient program created to calculate n prime no, which even continew's from last no found

--- math/n prime no.py
+++ math/n prime no.py
@@ -1,6 +1,7 @@
-# 25,000Th prime no = 287117.00
-# 50,000Th prime no = 611953.00 by (Willans)
+# last prime no 2L10Th up till found 28,98,527
+
 import math
+num = int(input("input the no."))
 
 # Type 1
 """
@@ -25,10 +26,8 @@ print(len(asq))
 print("thx")
 """
 
-# Type 2
-
-num = int(input("input the no."))
-# c = 287118 #to continue from 25,000
+# Type 2 squre root
+"""
 c = 2
 while num != 0:
     for i in range(2, int(math.sqrt(c))):
@@ -39,6 +38,8 @@ while num != 0:
         num -= 1
     c += 1
 print("\n")
+"""
+
 # Type 3 Willans prime detecting formula.
 """
 n = int(input("Enter n:- "))
@@ -55,13 +56,51 @@ while n > 0:
 # file1.close()
 """
 
+# Type 4 my type of formula
+file0 = open("2Lakh10Th prime no.txt", "a+")
+file0.seek(0)
+arr = file0.readline().split()
+primes = [int(no) for no in arr]
+a = primes[len(primes)-1]+1
+file0.seek(0, 2)
+while num != 0:
+    checker = 0
+    for u in primes:
+        if u > math.sqrt(a):
+            break
+        if a % u == 0:
+            checker += 1
+            break
+    if checker == 0:
+        print(a, end=" ")
+        file0.write(f"{a} ")
+        primes.append(a)
+        num -= 1
+    a += 1
+file0.close()
+
+# Type check1(for single no)
+"""
 y = 'y'
 while y == 'y':
     s = int(input("Enter No:- "))
-    for x in range(1,s+1):
-        if s%x == 0:
+    for x in range(1, s + 1):
+        if s % x == 0:
             print(x)
     y = input("Do you want to recheck, Enter 'y'/'n':- ")
+"""
+
+# Typ check2(for two files) Take's-Time to run
+"""
+file0 = open("2Lakh10Th prime no.txt", "r").readline().split()
+primes = [int(no) for no in file0]
+print(len(primes))
+for no in primes:
+    for n in range(2, int(math.sqrt(no))):
+        if no%n == 0:
+            print(no)
+            break
+"""
 
 print("\nthx")
 print("Have a nice day.")
