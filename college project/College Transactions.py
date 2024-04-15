c125364fa331b3ad98c41a78f4e25a2d3bc128d9
Akshatchourey <achourey09@gmail.com>
2024-04-15 22:52:20 +0530

    College project is completed which seeams good, hope so i find internship fast.

--- college project/College Transactions.py
+++ college project/College Transactions.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -9,12 +10,12 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("College Transactions")
-        self.iconbitmap("logo.ico")
-        self.geometry("920x610+175+90")
+        # self.iconbitmap("logo.ico")
+        self.geometry("935x615+175+90")
         self.minsize(900,550)
         self.bind('<Escape>', lambda event: self.quit())
 
-        menu = tk.Frame(self, borderwidth=4)
+        menu = ttk.Frame(self, borderwidth=4)
         menu.pack(side='left', fill='y')
 
         title = ttk.Label(menu, text="College Transactions", font="comic 12 bold")
@@ -27,8 +28,8 @@ class App(tk.Tk):
         add.pack()
         analyse.pack()
 
-        container = tk.Frame(self, bg="#8AA7A9")
-        container.pack(side="top", fill="both", expand=True)
+        container = ttk.Frame(self)
+        container.pack(side="top", expand=True, fill="both")
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
@@ -47,30 +48,48 @@ class App(tk.Tk):
     def show_frame(self, frame):
         self.frames[frame].tkraise()
 
-class HomePage(tk.Frame):
+class HomePage(ttk.Frame):
     def __init__(self, parent, container):
         super().__init__(container)
 
-        label = tk.Label(self, text="Home Page", font=('Times', '20'))
-        label.pack()
+        self.background = Image.open("img1.jpg").resize((948,767))
+        self.background_tk = ImageTk.PhotoImage(self.background)
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(expand=True, fill='both')
+        self.canvas.create_image(0, 0, image=self.background_tk, anchor='nw')
+        self.canvas.bind('<Configure>', self.resize_image)
 
+    def resize_image(self, event):
+        image_ratio = 1.75
+        canvas_ratio = event.width / event.height
 
-class Add(tk.Frame):
+        if canvas_ratio > image_ratio:  # canvas is wider then image
+            width = event.width
+            height = int(event.width / image_ratio)
+        else:  # canvas is narrower then image
+            height = event.height
+            width = int(event.height * image_ratio)
+
+        new_background = self.background.resize((width, height))
+        self.background_tk = ImageTk.PhotoImage(new_background)
+        self.canvas.create_image(int(event.width/2), int(event.height/2), image=self.background_tk, anchor='center')
+
+class Add(ttk.Frame):
     def __init__(self, parent, container):
         super().__init__(container)
 
-        add_t = tk.Label(self, text="Add Transactions", font=('Times', '20'))
+        add_t = ttk.Label(self, text="Add Transactions", font=('Times', '20'))
         add_t.pack()
 
         courser.execute("select count(*) from entry;")
         self.no = tk.IntVar(value=courser.fetchone()[0]+1)
-        date_value = tk.StringVar(value="2024-mm-dd")
+        date_value = tk.StringVar(value="dd/mm/2024")
         note_value = tk.StringVar(value="note")
         type_value = tk.StringVar(value="Category")
         mode_value = tk.BooleanVar()
         amount_value = tk.DoubleVar()
 
-        add_f1 = tk.Frame(self)
+        add_f1 = ttk.Frame(self)
         add_f1.pack(fill='x')
         add_f1.columnconfigure((0,1,2,3,4,5,6), weight=1)
         add_f1.rowconfigure(0, weight=1)
@@ -84,24 +103,24 @@ class Add(tk.Frame):
         mode_offline = ttk.Radiobutton(add_f1, text="Offline", value=True, variable=mode_value)
         amount_entry = ttk.Entry(add_f1, textvariable=amount_value)
 
-        tk.Label(add_f1, textvariable=self.no).grid(row=0, column=0,rowspan=2, sticky='ew')
+        ttk.Label(add_f1, textvariable=self.no).grid(row=0, column=0,rowspan=2, sticky='ew')
         date_entry.grid(row=0, column=1,rowspan=2, sticky='ew')
         note_entry.grid(row=0, column=2,rowspan=2, sticky='ew')
         mode_online.grid(row=0, column=3)
         mode_offline.grid(row=1, column=3)
         type_entry.grid(row=0, column=4,rowspan=2, sticky='ew')
         amount_entry.grid(row=0, column=5,rowspan=2, sticky='ew')
-        add_data = ttk.Button(add_f1, text="Add Data"
-                              ,command=lambda: self.add_data(date_value,note_value,type_value,mode_value,amount_value))
+        add_data = ttk.Button(add_f1, text="Add Data",
+                              command=lambda: self.add_data(date_value,note_value,type_value,mode_value,amount_value))
         add_data.grid(row=0, column=6, rowspan=2)
 
-        show_t = tk.Label(self, text="Show Transactions", font=('Times', '20'))
+        show_t = ttk.Label(self, text="Show Transactions", font=('Times', '20'))
         show_t.pack()
-        add_f2 = tk.Frame(self)
+        add_f2 = ttk.Frame(self)
         add_f2.pack()
 
-        refresh = ttk.Button(add_f2, text="Refresh Data"
-                             ,command=lambda:   self.show_data("select * from entry order by Date desc;"))
+        refresh = ttk.Button(add_f2, text="Refresh Data",
+                             command=lambda:self.show_data("select * from entry order by Date desc;"))
         refresh.pack(side='left')
 
         dicti = {
@@ -129,16 +148,19 @@ class Add(tk.Frame):
                         lambda event: self.show_data(
                             f"select * from entry where Type='{type_combo.get()}' order by Date desc;"))
 
-        self.tree = ttk.Treeview(self, columns=('sno', 'date', 'note', 'category', 'amount'), show='headings')
+        self.tree = ttk.Treeview(self, columns=('sno', 'date', 'note', 'category', 'amount'), show='headings', height=5)
         self.tree.column("sno", width=50)
-        self.tree.column("date", width=80)
+        self.tree.column("date", width=95)
+        self.tree.column('note', width=210)
         self.tree.heading('sno', text='SNo.')
         self.tree.heading('date', text='Date')
         self.tree.heading('note', text='Note')
         self.tree.heading('category', text='Category')
         self.tree.heading('amount', text='Amount')
         self.tree.pack(expand=True,fill='both',padx=9, pady=9)
+        ttk.Style().configure('Treeview', rowheight=28)
         self.tree.tag_configure("colour_blue", foreground="blue")
+        self.tree.tag_configure("tree_font", font='None 13')
 
         self.show_data("select * from entry order by Date desc;")
 
@@ -147,25 +169,27 @@ class Add(tk.Frame):
         courser.execute(query)
         i = 1
         for t in courser:
-            row = (i, t[1], t[2], t[3], t[5])
-            if t[4]: self.tree.insert('', 'end', values=row, tags=('colour_blue',))
-            else: self.tree.insert('', 'end', values=row)
+            old_date = str(t[1])
+            new_date = old_date[8:]+"/"+old_date[5:7]+"/"+old_date[0:4]
+            row = (i, new_date, t[2], t[3], t[5])
+            if t[4]: self.tree.insert('', 'end', values=row, tags=('colour_blue',"tree_font",))
+            else: self.tree.insert('', 'end', values=row, tags=("tree_font",))
             i += 1
 
     def add_data(self, date_value, note_value, type_value, mode_value, amount_value):
-        courser.execute(f"insert into entry values({self.no.get()},'{date_value.get()}','{note_value.get()}'"
-                        f",'{type_value.get()}',{mode_value.get()},{amount_value.get()});")
+        courser.execute(f"insert into entry values({self.no.get()},STR_TO_DATE('{date_value.get()}', '%d/%m/%Y'),"
+                        f"'{note_value.get()}','{type_value.get()}',{mode_value.get()},{amount_value.get()});")
         db.commit()
         self.no.set(self.no.get() + 1)
         self.show_data("select * from entry order by Date desc;")
 
 
-class Analyse(tk.Frame):
+class Analyse(ttk.Frame):
     def __init__(self, parent, container):
         super().__init__(container)
 
-        label = tk.Label(self, text="Analyzing Page", font=('Times', '20'))
-        frame3 = tk.Frame(self)
+        label = ttk.Label(self, text="Analyzing Page", font=('Times', '20'))
+        frame3 = ttk.Frame(self)
         label.pack()
         frame3.pack()
         dect = {
